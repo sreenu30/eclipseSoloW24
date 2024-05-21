884d181a72ec5fd879d8b4eb2b1ffe038371c0f7
sreenu <skunapar@emich.edu>
2024-05-21 10:31:31 +0300

    Changes to create merge conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("Now we cooking, aye?");
-		System.out.println("Programmer 2 did this.");
+		System.out.println("Programmer One did this.");
 		
 	}
 }

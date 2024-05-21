bd4eaa15f2e727a42505fee739a6d8e685c84106
sreenu <skunapar@emich.edu>
2024-05-21 09:28:40 +0300

    Commits by Programmer 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
-		System.out.println("Now we cooking, aye?")
+		System.out.println("Now we cooking, aye?");
+		System.out.println("Programmer 2 did this.");
 		
 	}
 }
